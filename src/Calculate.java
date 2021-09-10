82298c46e57b435abd7811c11a2b839ca0ab40d1
iwnlyfe <iwnlyfe@hotmail.com>
2021-09-11 00:47:12 +0700

    update code

--- src/Calculate.java
+++ src/Calculate.java
@@ -3,16 +3,17 @@ public class Calculate {
     private String employeeId;
     private String employeeName;
     private int salary;
-    private Double sales;
+    private double sales;
     
     public Calculate(){
 
     }
 
-    public Calculate(String employeeId, String employeeName, int salary){
+    public Calculate(String employeeId, String employeeName, int salary, double sales){
         this.employeeId = employeeId;
         this.employeeName = employeeName;
         this.salary = salary;
+        this.sales = sales;
     }
 
     public String getEmployeeId(){
@@ -34,18 +35,19 @@ public class Calculate {
     public double calNetSalary(){
         double netSalary = 0.00;
         if(this.salary <= 10000){
-            if(this.sales <= 50000){
+            if(this.sales <= 50000 && this.sales >= 1){
                 netSalary = this.salary * 1.05;
             } else {
                 netSalary = this.salary * 1.10;
             }
         } else {
-            if(this.salary <= 100000) {
+            if(this.sales >= 1 && this.sales <= 100000){
                 netSalary = this.salary * 1.05;
             } else {
                 netSalary = this.salary * 1.10;
             }
         }
+            
         return netSalary;
     }
     
--- src/Input.java
+++ src/Input.java
@@ -9,7 +9,7 @@ public class Input {
 		String employeeId;
 		String employeeName;
 		int salary;
-		double sales;
+		double sales = 0;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter your ID : ");
 		employeeId = in.nextLine();
@@ -17,7 +17,7 @@ public class Input {
 		employeeName = in.nextLine();
 		System.out.print("Enter your salary : ");
 		salary = in.nextInt();
-		Calculate c = new Calculate(employeeId, employeeName, salary);
+		Calculate c = new Calculate(employeeId, employeeName, salary, sales);
 		int date = today.getDayOfMonth();
 		Calendar cal = Calendar.getInstance();
 		int lastDateOfMonth = cal.getActualMaximum(Calendar.DATE);
@@ -27,15 +27,16 @@ public class Input {
 			c.setSales(sales);
 			lateDate = true;
 		}
-		toString(employeeId, employeeName, salary);
+		toString(employeeId, employeeName, salary, sales);
 		in.close();
 	}
 
-	public String toString(String employeeId, String employeeName, int salary){
-		Calculate c = new Calculate(employeeId, employeeName, salary);
+	public String toString(String employeeId, String employeeName, int salary, double sales){
+		Calculate c = new Calculate(employeeId, employeeName, salary, sales);
 		String employee = c.toString();
 		if(lateDate == true) {
 			employee = employee + " netSalary : " + c.calNetSalary();
+			
 		}
 		System.out.println(employee);
 		return employee;
